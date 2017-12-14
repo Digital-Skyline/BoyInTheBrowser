35097615be146121963e17312f55f865cbc0f57c
Thomas <thomasw2326@gmail.com>
2017-12-13 16:45:02 -0800

    Removed Backup folder

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'admin');
-   define('DB_PASSWORD', '123456789');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
    define('DB_DATABASE', 'boyinthebrowser');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    if ($db->connect_error) die($db->connect_error);
